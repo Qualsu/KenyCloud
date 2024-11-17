067e573778620327b09ff7b54dcc4635cb5ca9dd
keeniGitHub <maratkanovartem89@mail.ru>
2024-11-17 16:53:54 +0300

    add alert and change font in input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Alert } from "./landing/alert"
 import { Footer } from "./landing/footer"
 import Main from "./landing/main"
 
 export default async function Landing(){
     return (
         <div className="flex flex-col min-h-screen">
+            <Alert/>
             <main className="flex-grow flex items-center justify-center">
                 <Main/>
             </main>
